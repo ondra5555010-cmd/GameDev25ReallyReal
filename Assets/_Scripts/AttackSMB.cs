b67be88ac65df98dea3b1c93b3055dd3de3c4ba5
Rootment <m1_golan@utb.cz>
2025-11-05 15:19:07 +0100

    add smb need to be filled from github

--- Assets/_Scripts/AttackSMB.cs
+++ Assets/_Scripts/AttackSMB.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RunSMB : StateMachineBehaviour
+public class Attack : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
