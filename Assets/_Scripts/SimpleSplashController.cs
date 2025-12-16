48357bd56a3a6924fe48b1f0f735c5d9059ddb7a
Rootment <m1_golan@utb.cz>
2025-12-17 00:57:25 +0100

    add script and scene at index 0

---
+++ Assets/_Scripts/SimpleSplashController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Potřeba pro načítání scén
+using System.Collections; // Potřeba pro Coroutines (časovače)
+
+public class SimpleSplashController : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("Název scény s hlavním menu")]
+    public string sceneToLoad = "MainMenuScene";
+    
+    [Tooltip("Celková doba trvání splash screenu (musí odpovídat délce animace)")]
+    public float totalDuration = 3.0f;
+
+    void Start()
+    {
+        // Spustíme odpočet hned při startu scény
+        StartCoroutine(WaitAndLoadRoutine());
+    }
+
+    // Toto je "Korutina" - speciální metoda, která umí čekat
+    IEnumerator WaitAndLoadRoutine()
+    {
+        // Počkáme přesně tak dlouho, jak trvá animace
+        yield return new WaitForSeconds(totalDuration);
+        
+        // Po uplynutí času načteme další scénu
+        SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+}
